d0e942e0f665fdef98a6c0d7d7d20d5157daa4a0
atsushi <yoshino_mobio@yahoo.co.jp>
2019-09-26 15:45:28 +0900

    新規作成。Prism.Forms適用。

---
+++ CreateNew/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using CreateNew.Views;
+using CreateNew.ViewModels;
+
+namespace CreateNew
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ CreateNew/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace CreateNew.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Prism.Forms!";
+        }
+    }
+}
